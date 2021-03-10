f774c23357a1a7eca286fbf9f321880862d2438e
Jesse Wenger <wengerj@gmail.com>
2021-03-10 02:03:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
   # code goes here
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
   
   def initialize(name, owner)
     @name = name 
     @owner = owner
+    @mood = "nervous"
   end
 end
